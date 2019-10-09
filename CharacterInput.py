2467b64d404543dd017c39b191e63749da0926f5
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-10-09 16:08:27 +0000

    [ADD] EventOrOdd.js

--- CharacterInput.py
+++ CharacterInput.py
@@ -1,3 +1,2 @@
-#python version 3
-name = input("Give me yourname: ")
+name = input('Give me yourname: ')
 print("Your name is " + name)
--- README.md
+++
@@ -1,4 +0,0 @@
-### Python 101
-
-I'm practice python, 
-I'm interested.
